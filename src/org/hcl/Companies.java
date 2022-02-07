ece12ddde380085224d588250c9ccc4bc25ea6f2
swathikasubu <swathikasubu7@gmail.com>
2022-02-07 13:58:13 +0530

    swathi project

---
+++ src/org/hcl/Companies.java
@@ -0,0 +1,19 @@
+package org.hcl;
+
+
+
+public class Companies {
+
+	public void compId() {
+		System.out.println("company id is 67890");
+	}
+		public void companyName() {
+			System.out.println("company name is tcs");
+		}
+	public static void main(String[] args) {
+		Companies c = new Companies();
+		c.compId();
+		c.companyName();
+	}
+	}
+
